2620c2b3f8cb2eae53748121fa9627b31c682448
Amir Azizov <creator005@mail.ru>
2026-04-28 23:09:31 +0300

    commit 6

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,26 +1,26 @@
-import { getSession } from '@/entities/user'
-import { ImpersonationBanner } from '@/features/impersonation'
-import { WelcomeEmailDialog } from '@/features/welcome-email'
+// import { getSession } from '@/entities/user'
+// import { ImpersonationBanner } from '@/features/impersonation'
+// import { WelcomeEmailDialog } from '@/features/welcome-email'
 import { AppContainer } from '@/shared/ui/app-container'
 import { BottomBar } from '@/widgets/bottom-bar'
-import { Footer } from '@/widgets/footer'
-import { Header } from '@/widgets/header'
+// import { Footer } from '@/widgets/footer'
+// import { Header } from '@/widgets/header'
 
 export default async function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const { impersonation } = await getSession()
+  // const { impersonation } = await getSession()
 
   return (
     <div className="flex flex-col min-h-dvh">
-      <ImpersonationBanner
+      {/* <ImpersonationBanner
         isImpersonating={impersonation.isImpersonating}
         impersonationExpiresAt={impersonation.impersonationExpiresAt}
-      />
+      /> */}
 
-      <Header />
+      {/* <Header /> */}
 
       <main className="mt-5 grow">
         <AppContainer>{children}</AppContainer>
@@ -28,9 +28,9 @@ export default async function Layout({
 
       <BottomBar />
 
-      <Footer className="pb-22 md:pb-10" />
+      {/* <Footer className="pb-22 md:pb-10" /> */}
 
-      <WelcomeEmailDialog />
+      {/* <WelcomeEmailDialog /> */}
     </div>
   )
 }
